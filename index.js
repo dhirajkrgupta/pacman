de896b588f4d5f16f5f8edbf61eaa33ddae1e000
dhirajkrgupta <157622113+dhirajkrgupta@users.noreply.github.com>
2025-01-10 19:20:11 +0530

    movement optimization

--- index.js
+++ index.js
@@ -6,10 +6,21 @@ canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
 
 
+function DetectCollisionBetweenCircleAndRect(circle, rect) {
+    let closestX = Math.max(rect.position.x, Math.min(circle.position.x + circle.velocity.x, rect.position.x + rect.width));
+    let closestY = Math.max(rect.position.y, Math.min(circle.position.y + circle.velocity.y, rect.position.y + rect.height));
+
+    let distanceX = circle.position.x + circle.velocity.x - closestX;
+    let distanceY = circle.position.y + circle.velocity.y - closestY;
+
+    let distanceSquared = distanceX * distanceX + distanceY * distanceY;
+
+    return distanceSquared <= circle.radius * circle.radius;
+}
 
 
 class Boundary {
-    constructor({position}) {
+    constructor({ position }) {
         this.position = position;
         this.width = 40;
         this.height = 40;
@@ -20,14 +31,18 @@ class Boundary {
     }
 }
 
-class Player{
-    constructor({position, velocity}) {
+class Player {
+    constructor({ position, velocity }) {
         this.position = position;
         this.velocity = velocity;
         this.radius = 15;
         this.color = "red";
     }
 
+    setvelocity(x, y) {
+        this.velocity.x = x;
+        this.velocity.y = y;
+    }
     draw() {
         ctx.fillStyle = this.color;
         ctx.beginPath();
@@ -48,18 +63,18 @@ const map = [
     ['-', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '-'],
     ['-', '', '-', '-', '-', '-', '-', '-', '-'],
     ['-', ' ', ' ', ' ', '-', ' ', '-', '-', '-'],
-    ['-', ' ', '-', '-', ' ', ' ', ' ', ' ', '-'],
+    ['-', ' ', '-', ' ', ' ', ' ', ' ', ' ', '-'],
     ['-', ' ', ' ', ' ', ' ', '-', '-', '-', '-'],
     ['-', '-', '-', '-', '-', '-', '-', '-', '-'],
-  ];
-  
+];
+
 
 const boundaries = [];
-const player=new Player({position:{x:60,y:60},velocity:{x:0,y:0}})
-map.forEach((row,i) => {
-    row.forEach((symbol,j) => {
-        if (symbol === '-') { 
-            boundaries.push(new Boundary({position: {x: j*40, y: i*40}}));
+const player = new Player({ position: { x: 60, y: 60 }, velocity: { x: 0, y: 0 } })
+map.forEach((row, i) => {
+    row.forEach((symbol, j) => {
+        if (symbol === '-') {
+            boundaries.push(new Boundary({ position: { x: j * 40, y: i * 40 } }));
         }
     });
 })
@@ -70,15 +85,11 @@ function animate() {
     player.color = "red";
     boundaries.forEach((boundary) => {
         boundary.draw();
-        let top = boundary.position.y;
-        let bottom = boundary.position.y + boundary.height;
-        let left = boundary.position.x;
-        let right = boundary.position.x + boundary.width;
-        if (player.position.y+player.velocity.y>=top-player.radius && player.position.y+player.velocity.y<=bottom+player.radius && player.position.x+player.velocity.x>=left-player.radius && player.position.x+player.velocity.x<=right+player.radius) {
-            player.velocity.x = 0;
-            player.velocity.y = 0;
+        if (DetectCollisionBetweenCircleAndRect(player, boundary)) {
+            if (player.velocity.x !== 0) player.velocity.x = 0;
+            if (player.velocity.y !== 0) player.velocity.y = 0;
         }
-        
+
     });
     player.update();
 
@@ -86,45 +97,34 @@ function animate() {
 
 animate();
 
-window.addEventListener('keydown', ({key}) => {
-    switch (key) {
-        case 'w':
-            player.velocity.y = -1;
-            player.velocity.x = 0;
-            break;
-        case 'a':
-            player.velocity.x = -1;
-            player.velocity.y = 0;
-            break;
-        case 's':
-            player.velocity.y = 1;
-            player.velocity.x = 0;
-            break;
-        case 'd':
-            player.velocity.x = 1;
-            player.velocity.y = 0;
-            break;
-        default:
-            break;
+
+const keyMap = {
+    w: { x: 0, y: -1 },
+    a: { x: -1, y: 0 },
+    s: { x: 0, y: 1 },
+    d: { x: 1, y: 0 },
+};
+const activeKey = [];
+window.addEventListener('keydown', ({ key }) => {
+    if (key in keyMap && !activeKey.includes(key)) {
+        activeKey.push(key);
+        const { x, y } = keyMap[key];
+        player.setvelocity(x, y);
     }
 });
 
 
-window.addEventListener('keyup', ({key}) => {
-    switch (key) {
-        case 'w':
-            player.velocity.y = 0;
-            break;
-        case 'a':
-            player.velocity.x = 0;
-            break;
-        case 's':
-            player.velocity.y = 0;
-            break;
-        case 'd':
-            player.velocity.x = 0;
-            break;
-        default:
-            break;
+window.addEventListener('keyup', ({ key }) => {
+    if (key in keyMap) {
+        const index = activeKey.indexOf(key);
+        if (index > -1) activeKey.splice(index, 1);
+
+
+        if (activeKey.length !== 0) {
+            const { x, y } = keyMap[activeKey[activeKey.length - 1]]
+            player.setvelocity(x, y);
+        } else {
+            player.setvelocity(0, 0);
+        }
     }
- });
+});
